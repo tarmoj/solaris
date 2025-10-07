a02bb4ce9a00734b30f13ddd1b9d16de5bbe5f05
Tarmo on live.uuu <trmjhnns@gmail.com>
2025-10-07 18:33:19 +0300

    local init

---
+++ server/sslechoserver.h
@@ -0,0 +1,39 @@
+// Copyright (C) 2016 Kurt Pattyn <pattyn.kurt@gmail.com>.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
+#ifndef SSLECHOSERVER_H
+#define SSLECHOSERVER_H
+
+#include <QtCore/QObject>
+#include <QtCore/QList>
+#include <QtCore/QByteArray>
+#include <QtNetwork/QSslError>
+#include <QtNetwork/QSslCertificate>
+#include <QtNetwork/QSslKey>
+#include <QSslConfiguration>
+
+
+QT_FORWARD_DECLARE_CLASS(QWebSocketServer)
+QT_FORWARD_DECLARE_CLASS(QWebSocket)
+
+class SslEchoServer : public QObject
+{
+    Q_OBJECT
+public:
+    explicit SslEchoServer(quint16 port, QObject *parent = nullptr);
+    ~SslEchoServer() override;
+
+private Q_SLOTS:
+    void onNewConnection();
+    void processTextMessage(QString message);
+    void processBinaryMessage(QByteArray message);
+    void socketDisconnected();
+    void onSslErrors(const QList<QSslError> &errors);
+
+private:
+    QWebSocketServer *m_pWebSocketServer;
+    QList<QWebSocket *> m_clients;
+    bool prepareSsl(const QString &certPath, const QString &keyPath);
+    QSslConfiguration m_sslConfig;
+};
+
+#endif //SSLECHOSERVER_H
